ef1fa01d7a34c9caf57f95551d13bd4f8090a775
raccoonboy <raccoonboy0803@gmail.com>
2024-04-18 23:48:22 +0900

    #24.04.18 level-0 test(2)

---
+++ level-0/181852.js
@@ -0,0 +1,5 @@
+// 정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 제외한 수들을 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+function solution(num_list) {
+  return num_list.sort((a, b) => a - b).splice(5);
+}
